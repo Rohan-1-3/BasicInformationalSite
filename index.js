d37f3157c59367ad91829dd0886d95364db1247d
Rohan-1-3 <rohanshrestha130@gmail.com>
2024-05-05 21:03:42 +0545

    Completion of project

---
+++ index.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res)=>{
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    let path = "./views"
+    switch(req.url){
+        case "/":
+            path += "/index.html";
+            break;
+        case "/about":
+            path += "/about.html";
+            break;
+        case "/contact-me":
+            path += "/contact-me.html"
+            break;
+        default :
+            path += "/404.html"
+            break;
+    }
+    fs.readFile(path, (err, data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+
+            res.end(data);
+        }
+    })
+});
+
+server.listen(3000, "localhost", ()=>{
+    console.log("listening at port 3000")
+})
